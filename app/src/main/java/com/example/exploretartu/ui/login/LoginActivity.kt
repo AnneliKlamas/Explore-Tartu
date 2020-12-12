ad19f5bdbd5b9f357b9c4f0c5cde8f15f45db604
Enrih Sinilaid <enrih.sinilaid@gmail.com>
2020-12-13 00:59:45 +0200

    Added Registering user, email login and google login Closes #14, #15, #26

--- app/src/main/java/com/example/exploretartu/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/exploretartu/ui/login/LoginActivity.kt
@@ -1,12 +1,131 @@
 package com.example.exploretartu.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.exploretartu.MainActivity
 import com.example.exploretartu.R
+import com.example.exploretartu.ui.register.RegisterActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
+
+private lateinit var auth: FirebaseAuth
+private lateinit var mGoogleSignInClient: GoogleSignInClient
+private val RC_GOOGLE_SIGN_IN: Int = 100
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        auth = FirebaseAuth.getInstance()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
         setContentView(R.layout.activity_login)
+
+        if (FirebaseAuth.getInstance().currentUser != null){
+            startMainActivity()
+        }
+
+        bt_register.setOnClickListener {
+            startRegisterActivity()
+        }
+
+        bt_login_email.setOnClickListener {
+            startEmailLogin()
+        }
+
+        bt_login_google.setOnClickListener {
+            startGoogleLogin()
+        }
+    }
+
+    private fun startRegisterActivity(){
+        val intent: Intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun startMainActivity(){
+        val intent: Intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun startEmailLogin(){
+        auth.signInWithEmailAndPassword(et_login_email.text.toString(),
+            et_login_password.text.toString())
+            .addOnCompleteListener{task ->
+                if (task.isSuccessful){
+                    val user = auth.currentUser
+                    if (user != null) {
+                        Toast.makeText(this,
+                            "Successfully Logged in!\n" +
+                                    "Welcome back, ${user.displayName}!", Toast.LENGTH_LONG).show()
+                    }
+                    startMainActivity()
+                }
+                else {
+                    Toast.makeText(this, "Error when logging in, check credentials!", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun startGoogleLogin(){
+        var signinIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signinIntent, RC_GOOGLE_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_GOOGLE_SIGN_IN){
+            var task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handdleSigninResult(task)
+        }
+    }
+
+    private fun handdleSigninResult(task: Task<GoogleSignInAccount>) {
+        try {
+            var acc: GoogleSignInAccount? = task.getResult(ApiException::class.java)
+            Toast.makeText(this, "Signed in successfully", Toast.LENGTH_SHORT).show()
+            FirebaseGoogleAuth(acc)
+        }
+        catch (e: ApiException){
+            Toast.makeText(this, "Signin failed", Toast.LENGTH_SHORT).show()
+            FirebaseGoogleAuth(null)
+        }
+    }
+
+    private fun FirebaseGoogleAuth(acc: GoogleSignInAccount?) {
+        acc?.apply {
+            var authCredential: AuthCredential = GoogleAuthProvider.getCredential(acc.idToken, null)
+            auth.signInWithCredential(authCredential).addOnCompleteListener {
+                if (it.isSuccessful){
+                    val user = auth.currentUser
+                    if (user != null) {
+                        Toast.makeText(applicationContext, "Welcome back, ${user.displayName}!", Toast.LENGTH_SHORT).show()
+                    }
+                    startMainActivity()
+                }
+                else{
+                    Toast.makeText(applicationContext, "Unsuccessful", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/exploretartu/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/exploretartu/ui/register/RegisterActivity.kt
@@ -1,12 +1,69 @@
 package com.example.exploretartu.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.exploretartu.MainActivity
 import com.example.exploretartu.R
+import com.example.exploretartu.ui.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import kotlinx.android.synthetic.main.activity_register.*
+
+private const val TAG_REGISTER_EVENT = "Registering event"
+private lateinit var auth: FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
         setContentView(R.layout.activity_register)
+
+        bt_register_account.setOnClickListener {
+            val name: String = et_register_name.text.toString()
+            val email: String = et_register_email.text.toString()
+            val password: String = et_register_password.text.toString()
+            if (android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.isNotBlank() && name.isNotBlank()){
+                startRegisterNewUser(email, password, name)
+            }
+        }
+    }
+
+    private fun startRegisterNewUser(email: String, password: String, name: String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG_REGISTER_EVENT, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    //Lisa kasutaja kontole tema nimi juurde
+                    val profileUpdates = UserProfileChangeRequest.Builder().setDisplayName(name).build()
+                    user?.updateProfile(profileUpdates)?.addOnSuccessListener {
+                        Toast.makeText(this, "Welcome, ${user.displayName}", Toast.LENGTH_SHORT).show()
+                        startMainActivity()
+                    }
+                }
+                else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG_REGISTER_EVENT, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(this, "Registering failed!", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent: Intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun startMainActivity(){
+        val intent: Intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
