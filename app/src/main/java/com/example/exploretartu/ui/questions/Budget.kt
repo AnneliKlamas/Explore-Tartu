61ec5ec56302e7a45360eca2d1519110b8814223
Anneli Klamas <annelik98@gmail.com>
2020-12-13 23:27:12 +0200

    Closes #7

---
+++ app/src/main/java/com/example/exploretartu/ui/questions/Budget.kt
@@ -0,0 +1,12 @@
+package com.example.exploretartu.ui.questions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.exploretartu.R
+
+class Budget : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_budget)
+    }
+}
