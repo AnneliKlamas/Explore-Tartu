e01acaf5ecb3fa7634a6389432ab896e35417677
Anneli Klamas <annelik98@gmail.com>
2020-12-13 14:07:45 +0200

    Closes #iss3

--- app/src/main/java/com/example/exploretartu/ui/questions/Weather.kt
+++ app/src/main/java/com/example/exploretartu/ui/questions/Weather.kt
@@ -1,8 +1,10 @@
 package com.example.exploretartu.ui.questions
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.exploretartu.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class Weather : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
