eaefdc128650a4e92a6314340b74dd736f6cb79a
Anneli Klamas <annelik98@gmail.com>
2020-12-14 11:35:07 +0200

    Closes #44

--- app/src/main/java/com/example/exploretartu/MainActivity.kt
+++ app/src/main/java/com/example/exploretartu/MainActivity.kt
@@ -7,8 +7,6 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
