92c91676b3da33c29340f761f7bd907c50009d3a
Anneli Klamas <annelik98@gmail.com>
2020-12-14 15:57:48 +0200

    Closes #13

---
+++ app/src/main/java/com/example/exploretartu/ui/Congartulations.kt
@@ -0,0 +1,12 @@
+package com.example.exploretartu.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.exploretartu.R
+
+class Congartulations : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_congartulations)
+    }
+}
